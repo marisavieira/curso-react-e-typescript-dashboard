566adc111952fc487ea0a57fef624942ea51883d
Marisa <marisavrst@gmail.com>
2020-09-07 18:08:44 -0300

    commit 004 - criação do aside

--- src/components/Aside/index.tsx
+++ src/components/Aside/index.tsx
@@ -1,13 +1,51 @@
 import React from 'react';
+import {
+    MdDashboard,
+    MdArrowDownward,
+    MdArrowUpward,
+    MdExitToApp
+} from 'react-icons/md';
+
+import logoImg from '../../assets/logo.svg'
 
 import{
     Container,
+    Header,
+    LogoImg,
+    Title,
+    MenuContainer, 
+    MenuItemLink
 } from  './styles';
 
 const Aside: React.FC = () =>{
      return (
          <Container>
-             <h1> Aside </h1>
+             <Header>
+                 <LogoImg src={logoImg} alt="Logo Minha Carteira"/>
+                 <Title> Minha Carteira </Title>
+             </Header>
+
+             <MenuContainer>
+                <MenuItemLink href="#">
+                    <MdDashboard/>
+                    Dashboard
+                </MenuItemLink>
+                
+                <MenuItemLink href="#">
+                    <MdArrowDownward/>
+                    Entradas
+                </MenuItemLink>
+                
+                <MenuItemLink href="#">
+                    <MdArrowUpward/>
+                    Saídas
+                </MenuItemLink>
+                
+                <MenuItemLink href="#">
+                    <MdExitToApp/>
+                    Sair
+                </MenuItemLink>
+             </MenuContainer>
          </Container>
      );
 }
